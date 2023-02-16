bf530c3cda9643de4b9f1d3b2fa05b2467a89f82
cubemarin <m.v.kubynina@gmail.com>
2023-02-16 21:00:48 +0700

    Initial commit

---
+++ src/test/java/ru/netology/banklogin/data/DataHelper.java
@@ -0,0 +1,58 @@
+package ru.netology.banklogin.data;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.util.Locale;
+
+public class DataHelper {
+
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static String generateRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String generateRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static AuthInfo generateRandomUser() {
+        return new AuthInfo(generateRandomLogin(), generateRandomPassword());
+    }
+
+    public static VerificationCode getRandomVerificationCode() {
+        return new VerificationCode(faker.numerify("######"));
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AuthCode {
+        private String id;
+        private String user_id;
+        private String code;
+        private String created;
+    }
+}
---
+++ src/test/java/ru/netology/banklogin/page/LoginPage.java
@@ -0,0 +1,28 @@
+package ru.netology.banklogin.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.banklogin.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.page;
+
+public class LoginPage {
+    private final SelenideElement loginField = $("[data-test-id=login] input");
+    private final SelenideElement passwordField = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+    private final SelenideElement errorNotification = $("[data-test-id='error-notification']");
+
+    public void verifyErrorNotificationVisibility() {
+        errorNotification.shouldBe(visible);
+    }
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginField.setValue(info.getLogin());
+        passwordField.setValue(info.getPassword());
+        loginButton.click();
+        return page(VerificationPage.class);
+    }
+}
